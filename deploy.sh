1f3c13cd8607ca21e9ffc3f4f8d09090228d0a95
qixiaobro <982092690@qq.com>
2020-10-14 23:00:31 +0800

    自动化部署测试

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,40 @@
 #!/usr/bin/env sh
+
 # 确保脚本抛出遇到的错误
 set -e
-npm run build # 生成静态文件
-cd docs/.vuepress/dist # 进入生成的文件夹
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
 
 # deploy to github
-# echo 'blog.xugaoyi.com' > CNAME
-if [ -z "$ACCESS_TOKEN" ]; then
+# echo 'techfly.com' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:qixiaobro/qixiaobro.github.io.git
 else
-  msg='来自github action的自动部署'
-  githubUrl=https://qixiaobro:${ACCESS_TOKEN}@github.com/qixiaobro.github.io.git
+  msg='来自github actions的自动部署'
+  githubUrl=https://qixiaobro:${GITHUB_TOKEN}@github.com:qixiaobro/qixiaobro.github.io.git
   git config --global user.name "qixiaobro"
   git config --global user.email "982092690@qq.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master # 推送到github
+git push -f $githubUrl main:gh-pages # 推送到github
 
+# deploy to coding
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:xgy/xgy.git
+# else
+#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
 
-cd -
+cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
