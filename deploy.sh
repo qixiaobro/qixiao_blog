e75d0993749a88616a56958551cd4e38872c328a
qixiaobro <982092690@qq.com>
2020-10-14 21:45:09 +0800

    修改 ci

--- deploy.sh
+++ deploy.sh
@@ -10,15 +10,15 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github
-echo 'b.xugaoyi.com' > CNAME
+echo 'techfly.com' > CNAME
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+  githubUrl=git@github.com:qixiaobro/qixiao_blog.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+  githubUrl=https://qixiaobro:${GITHUB_TOKEN}@github.com:qixiaobro/qixiao_blog.git
+  git config --global user.name "qixiaobro"
+  git config --global user.email "982092690@qq.com"
 fi
 git init
 git add -A
