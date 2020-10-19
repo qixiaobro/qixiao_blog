c2bc98554ea2f8f7469aedee3b42fa813c049e7f
qixiaobro <982092690@qq.com>
2020-10-19 21:24:15 +0800

    fix:修改百度推送

--- baiduPush.sh
+++ baiduPush.sh
@@ -1,9 +1,13 @@
+###
+ # @Date: 2020-10-14 21:08:28
+ # @Description: 
+### 
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=www.techfly.top&token=NG5sgSPTswLX5v8D"
 
 rm -rf urls.txt # 灭迹
