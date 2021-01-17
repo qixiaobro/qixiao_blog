9d327f8468b4d605ef1e1c922392f30491079861
qixiaobro <982092690@qq.com>
2021-01-17 16:29:19 +0800

    fix:修改备案链接。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,41 +3,41 @@ const nav = require('./config/nav.js');
 module.exports = {
   title: "QiXiao's blog",
   description:
-    'web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。', // 描述,以 <meta> 标签渲染到页面html中
+    "web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。", // 描述,以 <meta> 标签渲染到页面html中
   // base: '/', // '/<github仓库名>/'， 默认'/'
   head: [
     // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+    ["link", { rel: "icon", href: "/img/favicon.ico" }], //favicons，资源放在public文件夹
     [
-      'meta',
+      "meta",
       {
-        name: 'keywords',
+        name: "keywords",
         content:
-          '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
+          "前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown",
       },
     ],
-    ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc' }], // 百度统计的站点拥有者验证
-    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    ["meta", { name: "baidu-site-verification", content: "7F55weZDDc" }], // 百度统计的站点拥有者验证
+    ["meta", { name: "theme-color", content: "#11a8cd" }], // 移动浏览器主题颜色
     // ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'}], // 移动端阻止页面缩放
   ],
   markdown: {
     lineNumbers: true, // 代码行号
   },
 
-  theme: 'vdoing', // 使用依赖包主题
+  theme: "vdoing", // 使用依赖包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
   themeConfig: {
     // 主题配置
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/EB-logo.png', // 导航栏logo
-    repo: 'https://github.com/qixiaobro', // 导航栏右侧生成Github链接
+    logo: "/img/EB-logo.png", // 导航栏logo
+    repo: "https://github.com/qixiaobro", // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
-    lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
-    docsDir: 'docs', // 编辑的文件夹
+    lastUpdated: "上次更新", // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+    docsDir: "docs", // 编辑的文件夹
     editLinks: true, // 启用编辑
-    editLinkText: '编辑',
+    editLinkText: "编辑",
 
     //*** 以下配置是Vdoing主题改动和新增的配置 ***//
 
@@ -67,33 +67,33 @@ module.exports = {
     // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
     // pageButton: false, // 是否显示快捷翻页按钮，默认true
 
-    sidebar: 'structuring', // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+    sidebar: "structuring", // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
 
     author: {
       // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-      name: 'QiXiao', // 必需
-      link: 'https://github.com/qixiaobro', // 可选的
+      name: "QiXiao", // 必需
+      link: "https://github.com/qixiaobro", // 可选的
     },
     blogger: {
       // 博主信息，显示在首页侧边栏
       avatar:
-        'https://avatars3.githubusercontent.com/u/34623348?s=460&u=4faff092a9ccc8e925566b416bef5597f209d319&v=4',
-      name: 'QiXiao',
-      slogan: '少抱怨，多思考，未来更美好。',
+        "https://avatars3.githubusercontent.com/u/34623348?s=460&u=4faff092a9ccc8e925566b416bef5597f209d319&v=4",
+      name: "QiXiao",
+      slogan: "少抱怨，多思考，未来更美好。",
     },
     social: {
       // 社交图标，显示于博主信息栏和页脚栏
       // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
       icons: [
         {
-          iconClass: 'icon-youjian',
-          title: '发邮件',
-          link: 'mailto:982092690@qq.com',
+          iconClass: "icon-youjian",
+          title: "发邮件",
+          link: "mailto:982092690@qq.com",
         },
         {
-          iconClass: 'icon-github',
-          title: 'GitHub',
-          link: 'https://github.com/qixiaobro',
+          iconClass: "icon-github",
+          title: "GitHub",
+          link: "https://github.com/qixiaobro",
         },
         // {
         //   iconClass: 'icon-erji',
@@ -105,7 +105,8 @@ module.exports = {
     footer: {
       // 页脚信息
       createYear: 2020, // 博客创建年份
-      copyrightInfo: 'QiXiao| 闽ICP备20003477号-1', // 博客版权信息，支持a标签
+      copyrightInfo:
+        '<a href="https://beian.miit.gov.cn/" target="_self">QiXiao| 闽ICP备20003477号-1</a>', // 博客版权信息，支持a标签
     },
   },
   plugins: [
@@ -114,61 +115,61 @@ module.exports = {
     //   color: '#11a8cd', // 爱心颜色，默认随机色
     //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
     // }],
-    'run',
+    "run",
     [
-      'thirdparty-search',
+      "thirdparty-search",
       {
         // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
         thirdparty: [
           // 可选，默认 []
           {
-            title: '在MDN中搜索',
-            frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-            behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+            title: "在MDN中搜索",
+            frontUrl: "https://developer.mozilla.org/zh-CN/search?q=", // 搜索链接的前面部分
+            behindUrl: "", // 搜索链接的后面部分，可选，默认 ''
           },
           {
-            title: '在Runoob中搜索',
-            frontUrl: 'https://www.runoob.com/?s=',
+            title: "在Runoob中搜索",
+            frontUrl: "https://www.runoob.com/?s=",
           },
           {
-            title: '在Vue API中搜索',
-            frontUrl: 'https://cn.vuejs.org/v2/api/#',
+            title: "在Vue API中搜索",
+            frontUrl: "https://cn.vuejs.org/v2/api/#",
           },
           {
-            title: '在Bing中搜索',
-            frontUrl: 'https://cn.bing.com/search?q=',
+            title: "在Bing中搜索",
+            frontUrl: "https://cn.bing.com/search?q=",
           },
           {
-            title: '通过百度搜索本站的',
-            frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
+            title: "通过百度搜索本站的",
+            frontUrl: "https://www.baidu.com/s?wd=site%3Axugaoyi.com%20",
           },
         ],
       },
     ],
 
-    'vuepress-plugin-baidu-autopush', // 百度自动推送
+    "vuepress-plugin-baidu-autopush", // 百度自动推送
 
     [
-      'one-click-copy',
+      "one-click-copy",
       {
         // 代码块复制按钮
         copySelector: [
           'div[class*="language-"] pre',
           'div[class*="aside-code"] aside',
         ], // String or Array
-        copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+        copyMessage: "复制成功", // default is 'Copy successfully and then paste it for use.'
         duration: 1000, // prompt message display time.
         showInMobile: false, // whether to display on the mobile side, default: false.
       },
     ],
     [
-      'demo-block',
+      "demo-block",
       {
         // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
         settings: {
           // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
           // cssLib: ['http://xxx'], // 在线示例中的css依赖
-          vue: 'https://unpkg.com/vue@next', // 在线示例中的vue依赖
+          vue: "https://unpkg.com/vue@next", // 在线示例中的vue依赖
           jsfiddle: false, // 是否显示 jsfiddle 链接
           codepen: true, // 是否显示 codepen 链接
           horizontal: false, // 是否展示为横向样式
@@ -176,22 +177,22 @@ module.exports = {
       },
     ],
     [
-      'vuepress-plugin-zooming', // 放大图片
+      "vuepress-plugin-zooming", // 放大图片
       {
-        selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
+        selector: ".theme-vdoing-content img:not(.no-zoom)", // 排除class是no-zoom的图片
         options: {
-          bgColor: 'rgba(0,0,0,0.6)',
+          bgColor: "rgba(0,0,0,0.6)",
         },
       },
     ],
     [
-      'vuepress-plugin-baidu-tongji', // 百度统计
+      "vuepress-plugin-baidu-tongji", // 百度统计
       {
-        hm: '503f098e7e5b3a5b5d8c5fc2938af002',
+        hm: "503f098e7e5b3a5b5d8c5fc2938af002",
       },
     ],
     [
-      'vuepress-plugin-comment', // 评论
+      "vuepress-plugin-comment", // 评论
       // {
       //   choosen: 'valine',
       //   options: {
@@ -202,30 +203,30 @@ module.exports = {
       //   }
       // },
       {
-        choosen: 'gitalk',
+        choosen: "gitalk",
         options: {
-          clientID: '2b5fa0ba553c9eb86848',
-          clientSecret: '06a275700bddd003d1a547fa90927292e80edaaa',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'qixiaobro', // GitHub仓库所有者
-          admin: ['qixiaobro'], // 对仓库有写权限的人
+          clientID: "2b5fa0ba553c9eb86848",
+          clientSecret: "06a275700bddd003d1a547fa90927292e80edaaa",
+          repo: "blog-gitalk-comment", // GitHub 仓库
+          owner: "qixiaobro", // GitHub仓库所有者
+          admin: ["qixiaobro"], // 对仓库有写权限的人
           // distractionFreeMode: true,
-          pagerDirection: 'last', // 'first'正序 | 'last'倒序
+          pagerDirection: "last", // 'first'正序 | 'last'倒序
           id:
-            '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-          title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-          labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
+            "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
+          title: "「评论」<%- frontmatter.title %>", // GitHub issue 的标题
+          labels: ["Gitalk", "Comment"], // GitHub issue 的标签
           body:
-            '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+            "页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>", // GitHub issue 的内容
         },
       },
     ],
     [
-      '@vuepress/last-updated', // "上次更新"时间格式
+      "@vuepress/last-updated", // "上次更新"时间格式
       {
         transformer: (timestamp, lang) => {
-          const moment = require('moment') // https://momentjs.com/
-          return moment(timestamp).format('YYYY/MM/DD, H:MM:SS')
+          const moment = require("moment"); // https://momentjs.com/
+          return moment(timestamp).format("YYYY/MM/DD, H:MM:SS");
         },
       },
     ],
@@ -238,5 +239,5 @@ module.exports = {
   //     }
   //   }
   // }
-}
+};
 
